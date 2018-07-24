c3c07cd6ccaba61d0dd9ff8b8d87cbcfa72350ab
Alexey Nesterov <nesterov@icerockdev.com>
2018-07-24 11:17:47 +0700

    BRRN Add empty Search component

---
+++ src/components/Search/index.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF',
+  },
+  centerText: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    height: 36,
+  },
+});
+
+export interface Props { }
+export interface State { }
+
+class Search extends React.Component<Props, State> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.centerText}>
+          Empty Search
+        </Text>
+      </View>
+    );
+  }
+}
+
+export default Search;
