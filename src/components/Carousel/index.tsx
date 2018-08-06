d6063cfd196c86c36385ddba3ee86bbf3f93c8d6
Alexey Nesterov <nesterov@icerockdev.com>
2018-08-06 12:42:03 +0700

    BRRN-1 Code style

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
