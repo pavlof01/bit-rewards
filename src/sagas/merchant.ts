4cf97e19c5124de8e95f4cb3aee64c2c5a5293f8
Alexey Nesterov <nesterov@icerockdev.com>
2018-08-14 17:08:26 +0700

    BRRN-30 Add merchant saga

---
+++ src/sagas/merchant.ts
@@ -0,0 +1,18 @@
+import { call, put, takeLatest } from 'redux-saga/effects';
+import Api from '../services/Api';
+import * as merchantActions from '../actions/merchant';
+
+function* merchantWalletFlow(action: historyActions.HistoryActions) {
+  try {
+    // TODO: remove hardcoded token when auth api was ready.
+    Api.setAuthToken('12345');
+    const responseData = yield call(Api.getMerchantData, action.payload.partnerKey);
+    yield put(merchantActions.MerchantActions.fetchWalletMerchantInfoSuccess(responseData.data));
+  } catch (err) {
+    yield put(merchantActions.MerchantActions.fetchWalletMerchantInfoFailure(err));
+  }
+}
+
+export function* merchantWalletData() {
+  yield takeLatest(merchantActions.FETCH_WALLET_MERCHANT_INFO_STARTED, merchantWalletFlow);
+}
